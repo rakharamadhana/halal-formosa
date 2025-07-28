9e24820ff69e21791ef6c3dc6eb67955070837cb
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2025-07-29 03:29:10 +0800

    Minor fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import { supabase } from '@/plugins/supabaseClient'; // your supabase instance
 
 // Preload SearchView in the background
 import SearchView from '@/views/SearchView.vue';
+import ExploreView from '@/views/ExploreView.vue.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,7 +13,7 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       { path: '', redirect: '/search' },
       { path: 'search', component: SearchView },
-      { path: 'explore', component: () => import('@/views/ExploreView.vue.vue') },
+      { path: 'explore', component: ExploreView },
       { path: 'add', component: () => import('@/views/AddProductView.vue'), meta: { requiresAuth: true } },
       { path: 'profile', component: () => import('@/views/ProfileView.vue') },
     ],
