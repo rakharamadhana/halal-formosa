ba2e7dd32d3678f3d3d78654d44a7f2cf29da445
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2025-08-03 00:24:58 +0800

    Release v1.0.7 to android

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -122,7 +122,7 @@
             </ion-item>
             <ion-item>
               <ion-label>Version</ion-label>
-              <ion-note slot="end">1.0.5</ion-note> <!-- Replace with actual version if dynamic -->
+              <ion-note slot="end">1.0.7</ion-note> <!-- Replace with actual version if dynamic -->
             </ion-item>
           </ion-list>
         </ion-card-content>
